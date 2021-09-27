4957a672e0da12ebcca45112c11380bcce327049
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2021-09-27 14:16:08 +0700

    add profesi, persyaratan, and step

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,22 @@ $(window).on("scroll", function (e) {
     }
     e.preventDefault()
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".btn-slider-profesi",
+    },
+});
+
+var swiper2 = new Swiper(".mySwiperStep", {
+    direction: "vertical",
+    pagination: {
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".bxs-down-arrow",
+        prevEl: ".bxs-up-arrow"
+    },
+});
